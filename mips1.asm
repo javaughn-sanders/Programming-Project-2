bc89117eb75c9b7af458749067d393ad1a997f70
javaughn-sanders <javaughnd.sanders@gmail.com>
2018-11-05 17:58:22 -0500

    Update 2 print successful

--- mips1.asm
+++ mips1.asm
@@ -7,3 +7,7 @@ main:
 	la      $a0, UserInput  #load UserInput from emeory and store in argument reqister 0
 	li      $a1, 64  	# how much memory to give user to input string
 	syscall
+    
+	la $a0, UserInput 	#load address UserInput from memory and store it into arguement register 0
+	li $v0, 4 		#loads the value 4 into register $v0 which is the op code for print string
+	syscall
